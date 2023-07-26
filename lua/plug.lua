f8d93c0d7af0c34bcff3459a0efcf4e6b0c69dc6
liu.bian.shi <liu.bian.shi@gmail.com>
2023-07-26 08:52:59 +0800

    add luarocks path to nvim

--- lua/plug.lua
+++ lua/plug.lua
@@ -1,5 +1,8 @@
 -- vim: set foldmethod=marker:
 -- 使用 lazyvim 加载插件
+package.path = package.path .. ";"
+            .. vim.env.HOME .. "/.luarocks/share/lua/5.1/?/init.lua;"
+            .. vim.env.HOME .. "/.luarocks/share/lua/5.1/?.lua;"
 
 -- 在 lazyvim 尚未安装时安装 -------------------------------------------- {{{1
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
