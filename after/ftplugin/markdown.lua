069d8971bf29c4b6554066f7e2532d2a7ea3e11a
liu.bian.shi <liu.bian.shi@gmail.com>
2024-04-28 16:44:32 +0800

    markdown rmd

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -8,8 +8,19 @@ vim.keymap.set("n", '<localleader>rf', function()
         require('r.run').start_R('R')
     end)
     local cmp = require('cmp')
-    local cmp_sources = cmp.core.sources
-    cmp_sources[#cmp_sources + 1] = { name = "cmp_r", group_index = 2 }
-    cmp.setup.buffer { sources = cmp_sources }
+    local config = require('cmp.config')
+    local cmp_sources = {}
+    for _, s in pairs(cmp.core.sources) do
+        if config.get_source_config(s.name) and s:is_available() then
+            table.insert(cmp_sources, config.get_source_config(s.name))
+        end
+    end
+    cmp_sources[#cmp_sources + 1] = {
+        name = "cmp_r",
+        group_index = 1,
+        option = {filetypes = {'r', 'rmd', 'markdown'}},
+    }
+    vim.notify(vim.inspect(cmp.config.sources(cmp_sources) ))
+    cmp.setup.buffer( { sources = cmp.config.sources(cmp_sources) })
     vim.api.nvim_buf_set_option(0, "filetype", "markdown")
 end, { desc = "Start R.nvim"})
