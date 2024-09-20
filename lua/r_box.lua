45dd8498a193b4a5e7123d813ea27035c40136f2
liu.bian.shi <liu.bian.shi@gmail.com>
2024-09-21 00:50:17 +0800

    Add function to edit box script file

--- lua/r_box.lua
+++ lua/r_box.lua
@@ -16,6 +16,7 @@ end
 local insert_pkg = function()
   local box_lib = vim.env.R_BOX_LIBRARY or (vim.env.HOME .. "/Repositories/R-script/box")
   local bash_code = {
+    [[fd -t f -e r '.' R 2>/dev/null]],
     "cd '" .. box_lib .. "'",
     [[fd '[.][Rr]$']],
     [[Rscript --no-restore --no-save --no-init-file -e 'cat(installed.packages() |> rownames(), sep = "\n")']]
@@ -28,13 +29,99 @@ local insert_pkg = function()
   vim.ui.select( items, { prompt = "Select: "}, handle_choice )
 end
 
-local reload_pkg = function()
+local function get_box_pkg()
   local line = vim.fn.getline('.')
   if not line then return end
-  line = line:gsub("%b[]", ""):gsub(" #.*$", ""):gsub("%s", "")
-  local s, e = vim.regex([[[A-Za-z.][A-Za-z0-9_.]*)]]):match_str(line)
-  local lib = line:sub(s + 1, e - 1)
+  local lib = nil
+  local fun = nil
+  if line:find("^box::use") then
+    line = line:gsub("%b[]", ""):gsub(" #.*$", ""):gsub("%s", "")
+    local s, e = vim.regex([[[A-Za-z.][A-Za-z0-9_.]*)]]):match_str(line)
+    lib = line:sub(s + 1, e - 1)
+  else
+    vim.cmd.normal("t(")
+    line = line:sub(1, vim.fn.col('.'))
+    local s, e = vim.regex("[A-Za-z.][A-Za-z0-9_.]*\\ze\\$[A-Za-z.][A-Za-z0-9_.]*$"):match_str(line)
+    lib = line:sub(s + 1, e)
+    local fs, fe = vim.regex("[A-Za-z.][A-Za-z0-9_.]*\\$\\zs[A-Za-z.][A-Za-z0-9_.]*$"):match_str(line)
+    fun = line:sub(fs + 1, fe)
+  end
+  return lib, fun
+end
+
+local reload_pkg = function()
+  local lib = get_box_pkg()
+  if not lib then return end
   require('r.send').cmd("box::reload(" .. lib .. ")")
+end
+
+local function get_box_script(lib_fullname)
+  if not lib_fullname then return end
+  if not lib_fullname:find("%/") then return end
+
+  local filepath = lib_fullname
+  if not lib_fullname:find("^R%/") then
+    local box_lib = vim.env.R_BOX_LIBRARY or (vim.env.HOME .. "/Repositories/R-script/box")
+    filepath = box_lib .. "/" .. lib_fullname
+  end
+
+  if vim.uv.fs_stat(filepath .. ".r") then
+    filepath = filepath .. ".r"
+  else
+    filepath = filepath .. ".R"
+  end
+
+  return filepath
+end
+
+local function get_box_lib_fullname(line)
+  line = line or vim.fn.getline('.')
+  if not line:find("^box::use") then return end
+  line = line:gsub("%b[]", ""):gsub(" #.*$", ""):gsub("%s", "")
+  local s, e = vim.regex("^box::use(\\zs[^,)]*\\ze[,)]"):match_str(line)
+  if not s then return end
+  return line:sub(s + 1, e)
+end
+
+local edit_box_script = function(method, new)
+  local line = vim.fn.getline('.')
+  if not line then return end
+
+  local lib_fullname = get_box_lib_fullname(line)
+  local fun_name = nil
+  local lib
+  if not lib_fullname then
+    lib, fun_name = get_box_pkg()
+    if not lib then return end
+
+    local lines = vim.api.nvim_buf_get_lines(0, 0, vim.fn.line('.') - 1, true)
+    for _, l in ipairs(lines) do
+      local clib = get_box_lib_fullname(l)
+      if clib and clib:find(lib .. "$") then
+        lib_fullname = clib
+        break
+      end
+    end
+  end
+  if not lib_fullname then return end
+
+  method = method or "edit"
+  new = new or false
+  local filepath = get_box_script(lib_fullname)
+  if not filepath or (not new and not vim.uv.fs_stat(filepath)) then
+    return
+  end
+  vim.cmd(method .. " " .. filepath)
+  if fun_name then
+    for i = 0, vim.api.nvim_buf_line_count(0) - 1 do
+      local ln = vim.api.nvim_buf_get_lines(0, i, i + 1, false)
+      if ln[1] and ln[1]:find("^" .. fun_name) then
+        vim.api.nvim_win_set_cursor(0, {i + 1, 0})
+        return
+      end
+    end
+    vim.notify("Function " .. fun_name .. " not found", vim.log.levels.WARN)
+  end
 end
 
 local keymap = function(key, fun, des, mode)
@@ -50,6 +137,7 @@ end
 M.set_keymap = function()
   keymap("a", insert_pkg, "Box: Import package")
   keymap("r", reload_pkg, "Box: Reload package")
+  keymap("e", edit_box_script, "Box: Edit package")
 end
 
 return M
